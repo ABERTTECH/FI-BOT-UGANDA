15c8248d6d069b759a0fd618a8f38979e40d18df
Noolayko <rawahankwanga148@gmail.com>
2024-08-16 15:57:05 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,9 +57,9 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: '' + b64data });
 
                let SIGMA_MD_TEXT = `
-*𝐁𝐄𝐋𝐓𝐀𝐇-𝐌𝐃 𝐕𝟒.𝟎.𝟎 𝐋𝐀𝐓𝐄𝐒𝐓 PAIR CODE CONNECTED*
+*FI BOT 𝐕2.𝟎.𝟎 𝐋𝐀𝐓𝐄𝐒𝐓 PAIR CODE CONNECTED*
 
-> You have completed the first step of Deploying *BELTAH-MD*, Copy the above message and deploy your bot.Meanwhile you must follow the 4 steps. 
+> You have completed the first step of Deploying *FI BOT*, Copy the above message and deploy your bot.Meanwhile you must follow the 4 steps. 
 
 > 1st STEP
 *TAP THE LINK BELOW AND FOLLOW THE CHANNEL* 
@@ -67,21 +67,21 @@ router.get('/', async (req, res) => {
 
 > 2nd STEP
 *YOU MUST JOIN OUR GROUP FOR UPDATES ON THIS BOT*
-𓄂 *_https://chat.whatsapp.com/CQvUnqaDK4fBjgMXvFEAsI_*
+𓄂 *_https://chat.whatsapp.com/KxVZyInjirjHVmt8LCBtYv_*
 
 > 3rd STEP
 *FORK AND STAR 🌟 THE REPO FOR COURAGE*
-𓄂 *_https://github.com/Beltahmd/beltah-md_*
+𓄂 *_https://github.com/Noolayko/FI-BOT-UGANDA-_*
 
 > 4th STEP 
 *VISIT FOR TUTORIALS IF YOU DON'T KNOW HOW TO DEPLOY*
-𓄂 *_https://www.youtube.com/@Beltahtech2024_*
+𓄂 *_https://www.youtube.com/@noplayug_*
 
 *DEVELOPER: BELTAH TECH*
-𓄂 *_https://wa.me/254114141192_*
+𓄂 *_https://wa.me/256704376077_*
 
-> MADE ON EARTH BY BELTAH🇰🇪
-> 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐁𝐄𝐋𝐓𝐀𝐇 𝐓𝐄𝐂𝐇`
+> MADE ON EARTH BY DON TECH🇺🇬
+> 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 DON 𝐓𝐄𝐂𝐇`
  await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
  
 
